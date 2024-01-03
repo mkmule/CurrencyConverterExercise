2c8210e12f3f64f8c886901ec0b6713bba361122
Serhiy <serhiy.mos@gmail.com>
2024-01-03 22:55:56 +0300

    Create start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, useColorScheme, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {
-  ActivityIndicator,
-  MD2Colors,
-  PaperProvider,
-  Text,
-} from 'react-native-paper';
+import { SafeAreaView, ScrollView } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import TransferStartScreen from './screens/TransferStartScreen.tsx';
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App = (): React.JSX.Element => {
   return (
     <PaperProvider>
-      <SafeAreaView style={backgroundStyle}>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <MaterialCommunityIcons name="bell" size={30} color="#333" />
-            <ActivityIndicator animating={true} color={MD2Colors.red800} />
-            <Text variant="displayLarge">Display Large</Text>
-          </View>
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <TransferStartScreen />
         </ScrollView>
       </SafeAreaView>
     </PaperProvider>
   );
-}
+};
 
 export default App;
