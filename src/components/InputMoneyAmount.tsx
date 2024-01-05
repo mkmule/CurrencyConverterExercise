ec6cf030cc2735ff11f64a318e7283a5164e55e4
Serhiy <serhiy.mos@gmail.com>
2024-01-05 16:39:38 +0300

    Implement Money input with currency formatting using device local settings

--- src/components/InputMoneyAmount.tsx
+++ src/components/InputMoneyAmount.tsx
@@ -1,6 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
+import { getNumberFormatSettings } from 'react-native-localize';
+
+import { formatCurrencyNumber, parseFormattedNumber, splitFormattedNumber } from '../utils/conversion.ts';
 
 interface Props {
   value: number;
@@ -9,28 +12,72 @@ interface Props {
 }
 
 const InputMoneyAmount = ({ value, decimals, onChangeAmount }: Props): React.JSX.Element => {
-  // const [amount, setAmount] = React.useState<string>('');
+  const [displayValue, setDisplayValue] = useState('');
+  const numberFormatOptions = useMemo(() => getNumberFormatSettings(), []);
+  const formatCurrencyNum = useMemo(() => {
+    return (val: number) => formatCurrencyNumber(val, decimals, numberFormatOptions);
+  }, [decimals, numberFormatOptions])
+  const parseFormattedNum = useMemo(() => {
+    return (val: string) => parseFormattedNumber(val, decimals, numberFormatOptions);
+  }, [decimals, numberFormatOptions])
   const placeholder = useMemo(() => {
-    return Number(0).toLocaleString('en', {
-      minimumFractionDigits: decimals,
-      maximumFractionDigits: decimals,
-    });
+    return formatCurrencyNum(0);
   }, [decimals]);
 
   const onChange = (val: string) => {
-    // setAmount(val);
-    onChangeAmount(Number(val));
+    if (!val?.length) {
+      onChangeAmount(0);
+      setDisplayValue('');
+
+      return;
+    }
+
+    const [numInteger, numDecimals] = splitFormattedNumber(val, decimals, numberFormatOptions);
+    if (numDecimals?.length > decimals) {
+      return;
+    }
+
+    const valNumber = parseFormattedNum(val);
+    if (val == null || Number.isNaN(valNumber)) {
+      // Not a number
+      onChangeAmount(0);
+      return;
+    }
+
+    if (val.charAt(val.length - 1) === numberFormatOptions.decimalSeparator) {
+      // Allow separator add
+      setDisplayValue(`${formatCurrencyNumber(Number(numInteger), 0, numberFormatOptions)}${numberFormatOptions.decimalSeparator}`);
+      onChangeAmount(valNumber as number);
+
+      return;
+    }
+
+    setDisplayValue(`${formatCurrencyNumber(Number(numInteger), 0, numberFormatOptions)}${numDecimals?.length ? `${numberFormatOptions.decimalSeparator}${numDecimals}` : ''}`);
+    onChangeAmount(valNumber as number);
   };
+
+  const onBlur = () => {
+    if (!displayValue?.length) {
+      onChangeAmount(0);
+      setDisplayValue('');
+
+      return;
+    }
+
+    const valNumber = parseFormattedNum(displayValue);
+    setDisplayValue(formatCurrencyNum(valNumber || 0));
+  }
 
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
         placeholder={placeholder}
-        keyboardType="decimal-pad"
+        keyboardType="numeric"
         mode="outlined"
-        value={String(value)}
+        value={displayValue}
         onChangeText={onChange}
+        onBlur={onBlur}
       />
     </View>
   );
--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -1,3 +1,61 @@
 export const convert = (amout: number, rate: number, precision = 2): number => {
   return Number((amout * rate).toFixed(precision));
 };
+
+interface NumberFormatSettings {
+  decimalSeparator: string;
+  groupingSeparator: string;
+}
+
+const DEFAULT_NUMBER_FORMAT_SETTINGS = {
+  decimalSeparator: '.',
+  groupingSeparator: ',',
+};
+
+export const formatCurrencyNumber = (num: number, decimals: number, options: NumberFormatSettings = DEFAULT_NUMBER_FORMAT_SETTINGS) => {
+  if (num == null) {
+    return '';
+  }
+
+  const { decimalSeparator, groupingSeparator } = options;
+  const numRounded = num.toFixed(decimals);
+
+  if (decimals) {
+    const [numInteger, numDecimals] = numRounded.split('.');
+    return `${numInteger.replace(/(\d)(?=(\d{3})+(?!\d))/g, `$1${groupingSeparator}`)}${decimalSeparator}${numDecimals}`
+  }
+
+  return numRounded.replace(/(\d)(?=(\d{3})+(?!\d))/g, `$1${groupingSeparator}`)
+}
+
+export const splitFormattedNumber = (displayNum: string, decimals: number, options: NumberFormatSettings = DEFAULT_NUMBER_FORMAT_SETTINGS): [string, string] => {
+  if (decimals) {
+    let [numInteger, numDecimals = ''] = displayNum.split(options.decimalSeparator);
+    numInteger = numInteger.replace(options.groupingSeparator, '').replace(/\s/g, '');
+
+    return [numInteger, numDecimals]
+  }
+
+  return [displayNum.replace(options.groupingSeparator, '').replace(/\s/g, ''), ''];
+}
+
+export const parseFormattedNumber = (displayNum: string, decimals: number, options: NumberFormatSettings = DEFAULT_NUMBER_FORMAT_SETTINGS): number | null => {
+  if (decimals) {
+    let [numInteger, numDecimals = ''] = splitFormattedNumber(displayNum, decimals, options);
+
+    if (numInteger.length && numDecimals.length) {
+      return Number(`${numInteger}.${numDecimals}`)
+    } else if (numInteger.length || numDecimals.length) {
+      return Number(`${numInteger || '0'}.${numDecimals || '0'.repeat(decimals)}`)
+    }
+
+    return null
+  }
+
+  const [numInteger] = splitFormattedNumber(displayNum, decimals, options);
+  if (numInteger.length) {
+    return Number(displayNum.replace(options.groupingSeparator, ''));
+  }
+
+  return null;
+}
